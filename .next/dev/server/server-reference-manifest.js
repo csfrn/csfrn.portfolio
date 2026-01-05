384e25d0de54379c7bf9c88eef62b90db11a4bb5
Lucas Furlan <155035762+csfrn@users.noreply.github.com>
2026-01-05 00:31:48 +0000

    remove .next from repo

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hyydIfXNAIlEq1sDD0iDBgKb6ERJMC8lBbCd8yHMtsU=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_6c9a9b28._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/1dff6_next_dist_9a37ec2d._.js",
-  "static/chunks/components_2520a735._.js"
-],
-    source: "dynamic"
-});
